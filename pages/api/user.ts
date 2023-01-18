6344bd529905e2657ca2a3e6f41a20cd16f17f45
Mauricio Borawski <mauricio.borawski@gmail.com>
2023-01-18 13:18:27 -0300

    Finish basic logic for create user

---
+++ pages/api/user.ts
@@ -0,0 +1,40 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Prisma } from "@prisma/client";
+import { connectDB, prisma } from "./connectDB";
+import { UserRegisterData } from "../types/types";
+
+connectDB();
+
+export default async function main(req: NextApiRequest, res: NextApiResponse) {
+  let user: Prisma.UserCreateInput;
+  const userInput: UserRegisterData = req.body as UserRegisterData;
+
+  //?: Check to create a file for each method
+  //?: Use a switch and separate each method into his own function.
+
+  if (req.method === "GET") {
+    //TODO: Code something =)
+  }
+
+  if (req.method === "POST") {
+    user = {
+      name: userInput.name,
+      email: userInput.email,
+      //TODO: Encrypt the password
+      password: userInput.password,
+    };
+
+    await prisma.user
+      .create({ data: user })
+      .then(() => {
+        res.status(200).json({ status: "success" });
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).json({ error: "Something went wrong" });
+      })
+      .finally(() => {
+        prisma.$disconnect();
+      });
+  }
+}
--- pages/types/types.ts
+++ pages/types/types.ts
@@ -1,3 +1,10 @@
 export type HelpCommands = "help" | "-h";
 export type Commands = HelpCommands;
 export type CommandList = Array<Commands>;
+
+//Back End
+export type UserRegisterData = {
+    name: string,
+    email: string,
+    password: string,
+}
